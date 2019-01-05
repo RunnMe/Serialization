58e807a78f5b0ecb8f27e0637eb0888e7b30bd91
Albert Stepantsev <admin@pr-of-it.ru>
2019-01-05 13:41:21 +0300

    + 7.2.3, 7.3.3, 7.4.3 versions

--- tests/Serialization/Serializers/YamlTest.php
+++ tests/Serialization/Serializers/YamlTest.php
@@ -182,9 +182,12 @@ class YamlTest extends \PHPUnit_Framework_TestCase
         $obj->foo = 'bar';
         $obj->baz = 42;
 
-        $this->assertSame(
-            '!php/object:O:8:"stdClass":2:{s:3:"foo";s:3:"bar";s:3:"baz";i:42;}',
-            $serializer->encode($obj)
+        $this->assertContains(
+            $serializer->encode($obj),
+            [
+                '!php/object \'O:8:"stdClass":2:{s:3:"foo";s:3:"bar";s:3:"baz";i:42;}\'',
+                '!php/object:O:8:"stdClass":2:{s:3:"foo";s:3:"bar";s:3:"baz";i:42;}',
+            ]
         );
     }
 
@@ -210,9 +213,12 @@ class YamlTest extends \PHPUnit_Framework_TestCase
         $obj->foo = 'bar';
         $obj->baz = 42;
 
-        $this->assertSame(
-            '!php/object:C:13:"Runn\Core\Std":41:{a:2:{s:3:"foo";s:3:"bar";s:3:"baz";i:42;}}',
-            $serializer->encode($obj)
+        $this->assertContains(
+            $serializer->encode($obj),
+            [
+                '!php/object \'C:13:"Runn\Core\Std":41:{a:2:{s:3:"foo";s:3:"bar";s:3:"baz";i:42;}}\'',
+                '!php/object:C:13:"Runn\Core\Std":41:{a:2:{s:3:"foo";s:3:"bar";s:3:"baz";i:42;}}',
+            ]
         );
     }
 
